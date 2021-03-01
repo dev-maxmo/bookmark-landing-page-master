99a481331e70534aa9f7bd0af53a5f26e2c56e96
Max Trappe <max@MBP-von-Max.local>
2021-03-01 21:31:29 +0100

    Finished the mobile menu

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,8 @@
 const toggler = document.querySelector(".header__toggler");
 const mobileMenu = document.querySelector(".header__mobile-menu");
-const mobileLink = document.querySelector(".header__mobile-link");
+const mobileButton = document.querySelector(".header__mobile-menu-button");
+const headerSocialIcons = document.querySelectorAll(".header__socials-link");
+const mobileLink = document.querySelectorAll(".header__mobile-link");
 const body = document.querySelector("body");
 const headerLogo = document.querySelector(".header__logo-link");
 const togglerLine = document.querySelectorAll("span");
@@ -8,7 +10,7 @@ const togglerLine = document.querySelectorAll("span");
 toggler.addEventListener('click', () => {
     toggler.classList.toggle("open");
     mobileMenu.classList.toggle("active");
-    mobileLink.classList.toggle("vissible");
+    mobileButton.classList.toggle("vissible");
 
     if(mobileMenu.classList.contains("active")) {
         body.classList.add("noscroll");
@@ -19,10 +21,26 @@ toggler.addEventListener('click', () => {
 
     if(toggler.classList.contains("open")) {
         headerLogo.classList.add("white");
-        togglerLine.classList.add("bg-white");
+        togglerLine.forEach((e) => {
+            e.classList.add("bg-white");
+        });
+        mobileLink.forEach((e) => {
+            e.classList.add("vissible");
+        });
+        headerSocialIcons.forEach((e) => {
+            e.classList.add("vissible");
+        });
     }
     else {
         headerLogo.classList.remove("white");
-        togglerLine.classList.remove("bg-white");
+        togglerLine.forEach((e) => {
+            e.classList.remove("bg-white");
+        });
+        mobileLink.forEach((e) => {
+            e.classList.remove("vissible");
+        });
+        headerSocialIcons.forEach((e) => {
+            e.classList.remove("vissible");
+        });
     }
 });
